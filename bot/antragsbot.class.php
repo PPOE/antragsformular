2714fd41d5307b82a9a019fc74ea160e440317a2
verr <verr@piratenpartei.at>
2013-01-14 03:36:21 +0100

    sample api call, debug method

--- bot/antragsbot.class.php
+++ bot/antragsbot.class.php
@@ -5,16 +5,31 @@
 class AntragsBot {
 	private $target;
 	private $template;
+	private $bot;
 
 	public function __construct($target, $template) {
       $this->target = $target;
       $this->template = $template;
+      $this->bot = new MediaWikiBot();
    }
 
   	public function post($data) {
-  		
+  		$params = array('modules' => 'protect');
+  		$this->myprintr($this->bot->help($params));
   	}
 
+
+
+  	// FOR DEBUGGING:
+  	function myprintr($array) {
+  		foreach ($array as $key => $value) {
+  			if(is_array($value)) {
+  				echo "[".$key."] => ".$this->myprintr($value)."<br>";
+  			} else {
+  				echo "[".$key."] => ".nl2br($value)."<br>";
+  			}
+  		}
+  	}
 }
 
 ?>
