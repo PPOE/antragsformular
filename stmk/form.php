3ca1ac568aab62818ba12c5d1038069c5fe77930
verr <verr@piratenpartei.at>
2013-01-14 02:33:37 +0100

    basic form

--- stmk/form.php
+++ stmk/form.php
@@ -5,36 +5,43 @@
     <meta charset="UTF-8" />
 </head>
 <body>
-<fieldset><legend>Antrag erstellen</legend>
-<form action="addHaltestelle.php" method="post">
-    <table>
-    <tr><td>Titel</td><td>
-        <input type="text" size="40" name="titel">
-    </td></tr>
-    <tr><td>Bereich</td><td>
-        <select name="bereich">
-          <option value="Anfrage">Anfrage</option>
-          <option value="Antrag">Antrag</option>
-          <option value="EDV-Verwaltung">&raquo; EDV-Verwaltung</option>
-          <option value="Terminverwaltung">&raquo; Terminverwaltung</option>
-          <option value="Mitarbeiterverwaltung">&raquo; Mitarbeiterverwaltung</option>
-          <option value="Finanzgebarung">&raquo; Finanzgebarung</option>
-        </select>
-    </td></tr>
-    <tr><td>Antragsteller</td><td>
-        <input type="text" size="40" name="antragsteller">
-    </td></tr>
-    <tr><td>Antragsforderung</td><td>
-        <textarea rows="4" cols="50" name="antragsforderung"></textarea>
-    </td></tr>
-    <tr><td>Antragsbegr&uuml;ndung</td><td>
-        <textarea rows="4" cols="50" name="antragsbegründung"></textarea>
-    </td></tr>
-    <tr><td>
-            <input type="submit" name="submit" value="Erstellen">
-    </td></tr>
-    </table>
-</form>
-</fieldset>
+    <fieldset><legend>Antrag erstellen</legend>
+    <form action="index.php" method="post">
+        <table>
+        <?
+            if(isset($error)) {?>
+            <tr><td>
+                <font color="red"><?=$error;?></font>
+            </td></tr>
+            <?}
+        ?>
+        <tr><td>Titel:</td><td>
+            <input type="text" size="40" name="titel" value="<?= $data['titel']; ?>">
+        </td></tr>
+        <tr><td>Bereich:</td><td>
+            <select name="bereich">
+              <option value="Anfrage">Anfrage</option>
+              <option value="Antrag">Antrag</option>
+              <option value="EDV-Verwaltung">&raquo; EDV-Verwaltung</option>
+              <option value="Terminverwaltung">&raquo; Terminverwaltung</option>
+              <option value="Mitarbeiterverwaltung">&raquo; Mitarbeiterverwaltung</option>
+              <option value="Finanzgebarung">&raquo; Finanzgebarung</option>
+            </select>
+        </td></tr>
+        <tr><td>Antragsteller:</td><td>
+            <input type="text" size="40" name="antragsteller" value="<?= $data['antragsteller']; ?>">
+        </td></tr>
+        <tr><td>Antragsforderung:</td><td>
+            <textarea rows="15" cols="80" name="antragsforderung"><?= $data['antragsforderung']; ?></textarea>
+        </td></tr>
+        <tr><td>Antragsbegr&uuml;ndung:</td><td>
+            <textarea rows="15" cols="80" name="antragsbegruendung"><?= $data['antragsbegruendung']; ?></textarea>
+        </td></tr>
+        <tr><td>
+                <input type="submit" name="submit" value="Erstellen">
+        </td></tr>
+        </table>
+    </form>
+    </fieldset>
 </body>
 </html>
--- stmk/index.php
+++ stmk/index.php
@@ -2,5 +2,32 @@
 	require_once('../bot/mediawikibot.class.php');
 	require_once('../bot/config.php');
 
-	require_once('./form.php');
+	$submitted = isset($_POST['submit']);
+
+	$keys = array("titel",
+					"bereich",
+					"antragsteller",
+					"antragsforderung",
+					"antragsbegruendung");
+
+	if($submitted) {
+		foreach ($keys as $key) {
+			if(isset($_POST[$key]) AND trim($_POST[$key]) != "") {
+				$data[$key] = $_POST[$key];
+			} else {
+				$data[$key] = "";
+				$error = "Bitte alle Felder ausf&uuml;llen!";
+			}
+		}
+		if(!isset($error)) {
+			// DO THE BOT MAGIC!
+			echo "Antrag erfolgreich erstellt! ;-)";
+		}
+	} else {
+		foreach ($keys as $key) {
+			$data[$key] = "";
+		}
+	}
+
+	if(!$submitted OR ($submitted AND isset($error))) require_once('./form.php');
 ?>
